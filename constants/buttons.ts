4be5aa3c3d765ca57ba642e4ebf5c9860b3fa2ae
techwobwez25 <techwobwez25@gmail.com>
2022-10-09 16:36:02 -0400

    refactor: add constants

---
+++ constants/buttons.ts
@@ -0,0 +1,84 @@
+import { ButtonProps } from "../interfaces/button";
+
+export const BUTTONS: ButtonProps[] = [
+    {
+        text: "AC",
+        type: "secondary",
+    },
+    {
+        text: "+/-",
+        type: "secondary",
+    },
+    {
+        text: "%",
+        type: "secondary",
+    },
+    {
+        text: "/",
+        type: "primary",
+    },
+    {
+        text: "7",
+        type: "number",
+    },
+    {
+        text: "8",
+        type: "number",
+    },
+    {
+        text: "9",
+        type: "number",
+    },
+    {
+        text: "x",
+        type: "primary",
+    },
+    {
+        text: "4",
+        type: "number",
+    },
+    {
+        text: "5",
+        type: "number",
+    },
+    {
+        text: "6",
+        type: "number",
+    },
+    {
+        text: "-",
+        type: "primary",
+    },
+    {
+        text: "1",
+        type: "number",
+    },
+    {
+        text: "2",
+        type: "number",
+    },
+    {
+        text: "3",
+        type: "number",
+    },
+    {
+        text: "+",
+        type: "primary",
+    },
+    {
+        text: "<-",
+        type: "number",
+    },
+    {
+        text: "0",
+        type: "number",
+    },
+    {
+        text: ".",
+        type: "number",
+    },
+    {
+        text: "=",
+        type: "primary",
+    },
+];
---
+++ constants/themes.ts
@@ -0,0 +1,21 @@
+import { ThemeProps } from "../interfaces/theme";
+
+export const DARK: ThemeProps = {
+    screenBackground: "#22252D",
+    padBackground: "#292D36",
+    text: "#FFFFFF" /*  */,
+    buttonBackground: "#272B33",
+    primaryButton: "#EC6666",
+    secondaryButton: "#26F8D2",
+    inactiveButton: "#73757B",
+};
+
+export const LIGHT: ThemeProps = {
+    screenBackground: "#FFFFFF",
+    padBackground: "#F9F9F9",
+    text: "#292D36",
+    buttonBackground: "#F7F7F7",
+    primaryButton: "#F37979",
+    secondaryButton: "#35FBD7",
+    inactiveButton: "#E4E4E4",
+};
