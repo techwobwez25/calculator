dcad293af8a38685a779fdf388560911a4af6634
techwobwez25 <techwobwez25@gmail.com>
2022-10-09 16:35:53 -0400

    refactor: add interfaces

---
+++ interfaces/theme.ts
@@ -0,0 +1,9 @@
+export interface ThemeProps {
+    screenBackground: string;
+    padBackground: string;
+    text: string;
+    buttonBackground: string;
+    primaryButton: string;
+    secondaryButton: string;
+    inactiveButton: string;
+}
